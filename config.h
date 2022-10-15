6b3abf6c16982e92478fdb029bc3c98f3294a09d
danalexandru <danb.alexandru@gmail.com>
2022-10-15 17:57:48 +0300

    update: Added new font to config.h

--- config.h
+++ config.h
@@ -8,6 +8,7 @@ static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
     "Hack Nerd Font:pixelsize=14:style=Bold:antialias=true:autohint=true",
+    "JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
